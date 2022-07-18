85f8422d071ba34c540ba5409937d986f1f852a6
Xavi Aracil <xaracil@uoc.edu>
2022-07-18 11:23:02 +0200

    Update streamlit_app.py
    Let's Get the Fruityvice Data Looking a Little Nicer

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,3 +29,8 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# normalize response
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# display response
+streamlit.dataframe(fruityvice_normalized)
