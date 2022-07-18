269ce2b08ca7c442c115fa11e29a524e879d2b33
Xavi Aracil <xaracil@uoc.edu>
2022-07-18 11:24:16 +0200

    Update streamlit_app.py
    Remove log text

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,8 +27,7 @@ streamlit.dataframe(fruits_to_show)
 # Request
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 # normalize response
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
